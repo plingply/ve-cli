8e3a3dd8fdaa66d8a2d2d964bbae27df32f967a7
彭林 <plin@qq.com>
2018-07-21 15:55:59 +0800

    删除git仓库文件

--- bin/ve.js
+++ bin/ve.js
@@ -1,68 +1,56 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-var program = require("commander");
-var ora = require("ora");
-var exists = require("fs").existsSync;
-var rm = require("rimraf").sync;
-var path = require("path");
-var logger = require("../lib/logger");
-var chalk = require('chalk')
-var fs = require("fs")
+const program = require('commander');
+const chalk = require('chalk')
+const webpack2 = "https://git.coding.net/plingply/vue_webpack2.X.git";
+const webpack4 = "https://github.com/plingply/webpack4-template.git";
+const version = require('../package.json').version
+const logger = require("../lib/logger");
+const rm = require("rimraf").sync;
+const path = require("path");
+const fs = require("fs")
+const exists = fs.existsSync;
+const ora = require("ora");
 
 program
-    .usage("init [项目名称]")
-
-program.on("--help", function () {
-    console.log();
-    console.log("  例如:");
-    console.log(
-        chalk.gray("    # 创建新项目")
-    );
-    console.log("    $ ve init my-project");
-});
-
-var url = "https://git.coding.net/plingply/vue_webpack2.X.git";
+    .version(version)
+    .usage("webpack2 [项目名称]")
+    .option('-l, --look', '查看支持的webpack版本，以及资源路径')
+    .parse(process.argv);
 
 
-
-/**
- * Help.
- */
-
-function help() {
-    program.parse(process.argv)
-    if (program.args.length < 1) return program.help()
-}
-help()
+if (program.look) {
+    console.log(chalk.green('webpack2模板：' + webpack2))
+    console.log(chalk.green('webpack4模板：' + webpack4))
+};
 
 
-var args = program.args;
-
-function init() {
-    if (args[0] != "init") {
-        return program.help();
-    }
-    if (!args[2] || args[2] == 'v2') {
-        url = "https://git.coding.net/plingply/vue_webpack2.X.git";
-        console.log(chalk.green("    # v2模板地址：" + url));
-    }
-    downloadFun();
+// 下载webpack2 模板
+if (program.args.length > 0 && program.args[0] == 'webpack2' && program.args[1]) {
+    downloadFun(webpack2,'vue_webpack2.X')
 }
 
-function downloadFun() {
+// 下载webpack4 模板
+else if (program.args.length > 0 && program.args[0] == 'webpack4' && program.args[1]) {
+    downloadFun(webpack4,'webpack4-template')
+}
+
+else {
+    if (!program.look) {
+        program.help()
+    }
+}
+
+
+function downloadFun(url,name) {
     var spinner = ora("开始下载模板...");
     spinner.start();
     if (args[1]) {
         var tmp = path.join(process.cwd(), args[1]);
-        var oldpath = path.join(process.cwd(), 'vue_webpack2.X');
+        var oldpath = path.join(process.cwd(), name);
         if (exists(tmp)) rm(tmp);
         if (exists(oldpath)) rm(oldpath);
         var c = require("child_process");
         c.execSync("git clone " + url);
-        fs.rename(path.join(process.cwd(), 'vue_webpack2.X'), tmp, function (err) {
+        fs.rename(path.join(process.cwd(), name), tmp, function (err) {
             if (err) {
                 logger.fatal("rename err:" + err)
             }
@@ -71,14 +59,8 @@ function downloadFun() {
             spinner.stop();
             logger.fatal("下载完成")
         })
-    } else {
-        var oldpath = path.join(process.cwd(), 'vue_webpack2.X');
-        if (exists(oldpath)) rm(oldpath);
-        var c = require("child_process");
-        c.execSync("git clone " + url);
-        spinner.stop();
-        logger.fatal("安装完成")
     }
 }
 
-init();
+
+
---
+++ bin/vess.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+var program = require("commander");
+var ora = require("ora");
+
+var rm = require("rimraf").sync;
+var path = require("path");
+var logger = require("../lib/logger");
+var chalk = require('chalk')
+var fs = require("fs")
+var exists = fs.existsSync;
+var help = require("../lib/help")
+program
+    .version('0.0.1')
+    .option('-i, --init', 'Add bbq sauce')
+    .usage("init [项目名称]")
+    .parse(process.argv);
+
+program.on("--help", help);
+
+program.on("--init",function(){
+    console.log('--bbq')
+})
+
+
+
+var url = "https://git.coding.net/plingply/vue_webpack2.X.git";
+
+
+
+/**
+ * Help.
+ */
+
+function help() {
+    program.parse(process.argv)
+    if (program.args.length < 1) return program.help()
+}
+
+
+
+var args = program.args;
+
+function init() {
+    if (args[0] != "init") {
+        return program.help();
+    }
+    if (!args[2] || args[2] == 'v2') {
+        url = "https://git.coding.net/plingply/vue_webpack2.X.git";
+        console.log(chalk.green("    # v2模板地址：" + url));
+    }
+    downloadFun();
+}
+
+function downloadFun() {
+    var spinner = ora("开始下载模板...");
+    spinner.start();
+    if (args[1]) {
+        var tmp = path.join(process.cwd(), args[1]);
+        var oldpath = path.join(process.cwd(), 'vue_webpack2.X');
+        if (exists(tmp)) rm(tmp);
+        if (exists(oldpath)) rm(oldpath);
+        var c = require("child_process");
+        c.execSync("git clone " + url);
+        fs.rename(path.join(process.cwd(), 'vue_webpack2.X'), tmp, function (err) {
+            if (err) {
+                logger.fatal("rename err:" + err)
+            }
+
+            rm(tmp + "/.git")
+            spinner.stop();
+            logger.fatal("下载完成")
+        })
+    } else {
+        var oldpath = path.join(process.cwd(), 'vue_webpack2.X');
+        if (exists(oldpath)) rm(oldpath);
+        var c = require("child_process");
+        c.execSync("git clone " + url);
+        spinner.stop();
+        logger.fatal("安装完成")
+    }
+}
+
+// init();
