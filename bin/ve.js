ffb5d743d883f8a91177cb425aad4f7545fa1a13
penglin <plin@qq.com>
2019-04-28 10:02:07 +0800

    add.

--- bin/ve.js
+++ bin/ve.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var program = require('commander');
 var chalk = require('chalk')
 var webpack2 = "https://git.coding.net/plingply/vue_webpack2.X.git";
@@ -19,7 +20,6 @@ program
     .option('-l, --look', '查看支持的webpack版本，以及资源路径')
     .parse(process.argv);
 
-
 if (program.look) {
     console.log(chalk.green('webpack2模板：' + webpack2))
     console.log(chalk.green('webpack4模板：' + webpack4))
@@ -28,52 +28,46 @@ if (program.look) {
 
 // 下载webpack2 模板
 if (program.args.length > 0 && program.args[0] == 'webpack2' && program.args[1]) {
-    downloadFun(webpack2,'vue_webpack2.X')
+    downloadFun(webpack2, program.args[1])
 }
 
 // 下载webpack4 模板
 else if (program.args.length > 0 && program.args[0] == 'ssr' && program.args[1]) {
-    downloadFun(webpack3,'webpack3-vue-ssr')
+    downloadFun(webpack3, program.args[1])
 }
 
 // 下载webpack4 模板
 else if (program.args.length > 0 && program.args[0] == 'webpack4' && program.args[1]) {
-    downloadFun(webpack4,'webpack4-template')
+    downloadFun(webpack4, program.args[1])
 }
 
 // 下载小程序 模板
 else if (program.args.length > 0 && program.args[0] == 'wxapp' && program.args[1]) {
-    downloadFun(wxapp,'wxapp')
-}
-
-else {
+    downloadFun(wxapp, program.args[1])
+} else {
     if (!program.look) {
         program.help()
     }
 }
 
 
-function downloadFun(url,name) {
+function downloadFun(url, name) {
     var spinner = ora("开始下载模板...");
     spinner.start();
-    if (program.args[1]) {
-        var tmp = path.join(process.cwd(), program.args[1]);
-        var oldpath = path.join(process.cwd(), name);
-        if (exists(tmp)) rm(tmp);
-        if (exists(oldpath)) rm(oldpath);
-        var c = require("child_process");
-        c.execSync("git clone " + url);
-        fs.rename(path.join(process.cwd(), name), tmp, function (err) {
-            if (err) {
-                logger.fatal("rename err:" + err)
-            }
-
-            rm(tmp + "/.git")
-            spinner.stop();
-            logger.fatal("下载完成")
-        })
+    if (!name) {
+        logger.fatal("请输入项目名称")
+        return
     }
+    var oldpath = path.join(process.cwd(), name);
+    if (exists(oldpath)) {
+        logger.fatal("目录已存在")
+        return
+    };
+
+    var c = require("child_process");
+    c.execSync("git clone " + url + " " + name);
+    rm(oldpath + "/.git")
+    spinner.stop();
+    logger.success("下载完成")
+
 }
-
-
-
